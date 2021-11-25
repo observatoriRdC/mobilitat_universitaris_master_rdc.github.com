e6a612632c47bfa5c74a62e30e0e32d63f25c185
observatoriRdC <pousf@staperpetua.org>
2021-11-25 08:49:44 +0100

    Master

---
+++ data/EstudiantsdeMsteruniversitari_1.js
@@ -0,0 +1,19 @@
+var json_EstudiantsdeMsteruniversitari_1 = {
+"type": "FeatureCollection",
+"name": "EstudiantsdeMsteruniversitari_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "UNI": "UAB", "ESTUDIANTS": "49" }, "geometry": { "type": "Point", "coordinates": [ 2.106013317001866, 41.500355742861288 ] } },
+{ "type": "Feature", "properties": { "UNI": "UAO", "ESTUDIANTS": "3" }, "geometry": { "type": "Point", "coordinates": [ 2.126778026991307, 41.410253930002931 ] } },
+{ "type": "Feature", "properties": { "UNI": "UB", "ESTUDIANTS": "30" }, "geometry": { "type": "Point", "coordinates": [ 2.163726833463323, 41.387074320135397 ] } },
+{ "type": "Feature", "properties": { "UNI": "UdG", "ESTUDIANTS": "1" }, "geometry": { "type": "Point", "coordinates": [ 2.827190372662051, 41.985710754185256 ] } },
+{ "type": "Feature", "properties": { "UNI": "UdL", "ESTUDIANTS": "1" }, "geometry": { "type": "Point", "coordinates": [ 0.619638152736769, 41.614781425770055 ] } },
+{ "type": "Feature", "properties": { "UNI": "UIC", "ESTUDIANTS": "0" }, "geometry": { "type": "Point", "coordinates": [ 2.123532021706026, 41.405700906822737 ] } },
+{ "type": "Feature", "properties": { "UNI": "UOC", "ESTUDIANTS": "86" }, "geometry": { "type": "Point", "coordinates": [ 2.132846285255911, 41.414748053235101 ] } },
+{ "type": "Feature", "properties": { "UNI": "UPC", "ESTUDIANTS": "27" }, "geometry": { "type": "Point", "coordinates": [ 2.115413656865425, 41.387621194955699 ] } },
+{ "type": "Feature", "properties": { "UNI": "UPF", "ESTUDIANTS": "11" }, "geometry": { "type": "Point", "coordinates": [ 2.179799765374927, 41.378981550919747 ] } },
+{ "type": "Feature", "properties": { "UNI": "URL", "ESTUDIANTS": "11" }, "geometry": { "type": "Point", "coordinates": [ 2.166288300823854, 41.383528635389382 ] } },
+{ "type": "Feature", "properties": { "UNI": "URV", "ESTUDIANTS": "3" }, "geometry": { "type": "Point", "coordinates": [ 1.249953613699355, 41.122703091262942 ] } },
+{ "type": "Feature", "properties": { "UNI": "UVIC-UCC", "ESTUDIANTS": "6" }, "geometry": { "type": "Point", "coordinates": [ 2.247251447448373, 41.933735322249511 ] } }
+]
+}
